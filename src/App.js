f24c92b9747448a7a1685de3a9bc736bcd94d006
Ki Vorndran <drake.vorndran@students.makeschool.com>
2020-02-16 08:51:19 -0800

    added a splash screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './css/map.css'
 
 
 import MapView from "./screens/MapView"
+import Home from "./screens/Home"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 const App = function () {
@@ -12,7 +13,7 @@ const App = function () {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={Home} />
         <Route exact path="/maps/:search" component={MapView} />
       </Switch>
     </Router>
---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { Link } from "react-router-dom"
+
+import "../css/Home.css"
+
+function Home() {
+
+  return (
+    <div className="home">
+      <h1>Everyone Eats</h1>
+      <h4>Bridging the gap between waste and want</h4>
+      <div>
+        <Link to="/maps/donors"><button>Show Donors</button></Link>
+        <Link to="/maps/non-profits"><button>Show Non-Profits</button></Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
