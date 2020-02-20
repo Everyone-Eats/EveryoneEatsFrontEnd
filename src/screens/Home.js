370a9dec369429482ae9223c0d37b061c83e2197
Preston Whitehall <pmwhitehall@gmail.com>
2020-02-19 18:25:49 -0800

    Changed 'non-profits' to 'charities'

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,7 +12,7 @@ function Home() {
       <h4>Bridging the gap between waste and want</h4>
       <div>
         <Link to="/maps/donors"><button>Show Donors</button></Link>
-        <Link to="/maps/non-profits"><button>Show Non-Profits</button></Link>
+        <Link to="/maps/charities"><button>Show Charities</button></Link>
       </div>
     </div>
   )
