4213908225c348afeb57b9fb4a9af68b6f7cef98
Preston Whitehall <pmwhitehall@gmail.com>
2020-02-19 18:27:50 -0800

    Passes 'search' param to Map component

--- src/screens/MapView.js
+++ src/screens/MapView.js
@@ -1,19 +1,17 @@
 import React from "react"
-
 import Map from '../components/Map'
-
 import "../css/MapView.css"
 
 function MapView(props) {
+  const { search } = props.match.params;
   return(
     <div>
       {/* <div className="floating-container">
-
       </div> */}
-      <Map />
+      <Map search={search}/>
 
     </div>
-  )
-}
+  );
+};
 
 export default MapView
