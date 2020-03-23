d00b161399fc6fe1a1ed24b1d6c06921a991b834
ALau2088 <alau12004@yahoo.com>
2020-03-22 19:37:35 -0700

    WIP added websocket boilerplate

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import geolocation from "geolocation";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import geolocation from 'geolocation';
+import io from 'socket.io-client';
 
 class MapContainer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       locations: [],
       locationDivs: [],
-      center: undefined,
-    }
+      center: undefined
+    };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     geolocation.getCurrentPosition((err, pos) => {
       if (err) {
         console.log(err);
@@ -22,30 +23,44 @@ class MapContainer extends Component {
       const lat = pos.coords.latitude;
       const lng = pos.coords.longitude;
       const center = { lat, lng };
-      this.setState({ center })
-    })
+      this.setState({ center });
+    });
     const { search } = this.props;
     let type = search === 'donors' ? 'donor' : 'charity';
+    await this.getUsers(type);
 
-    axios.get('http://localhost:8000/api/users', { params: { type }})
-    .then(response => {
-      const { users } = response.data;
-      const locations = [];
-      users.forEach(user => {
-        locations.push([user.position.long, user.position.lat]);
+    /* websockets */
+    const socket = io.connect('http://localhost:8000/', {
+      transports: ['websocket']
+    });
+
+    socket.on('message', function(payload) {
+      console.log('socket.io received data', payload);
+    });
+  }
+
+  getUsers(type) {
+    axios
+      .get('http://localhost:8000/api/users', { params: { type } })
+      .then(response => {
+        const { users } = response.data;
+        console.log(users);
+        const locations = [];
+        users.forEach(user => {
+          locations.push([user.position.long, user.position.lat]);
+        });
+        this.setState({ locations });
       })
-      this.setState({ locations });
-    })
-    .catch(error => {
-      console.log(error);
-    })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   displayMarkers() {
     const { locations } = this.state;
     return locations.map((location, index) => {
-      const [ lng, lat ] = location;
-      return <Marker key={index} id={index} position={{lat, lng}} />
+      const [lng, lat] = location;
+      return <Marker key={index} id={index} position={{ lat, lng }} />;
     });
   }
 
@@ -56,20 +71,20 @@ class MapContainer extends Component {
     const zoom = center ? 14 : 1;
     return (
       <Map
-        google = { this.props.google }
-        zoom = { zoom }
-        style = { {
+        google={this.props.google}
+        zoom={zoom}
+        style={{
           width: '100%',
           height: '100%'
-        } }
-        center = { {lat: lat, lng: lng} }
+        }}
+        center={{ lat: lat, lng: lng }}
       >
         {this.displayMarkers()}
       </Map>
     );
   }
-};
+}
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY
 })(MapContainer);
